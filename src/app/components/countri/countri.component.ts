dc52f7ff248f268c09fcc751694f833582076c92
andresmauricio <andresacelas@outlook.com>
2021-03-02 20:30:20 -0500

    render information countrie

--- src/app/components/countri/countri.component.ts
+++ src/app/components/countri/countri.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-countri',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countri.component.scss']
 })
 export class CountriComponent implements OnInit {
-
+  @Input() countrie;
   constructor() { }
 
   ngOnInit(): void {
